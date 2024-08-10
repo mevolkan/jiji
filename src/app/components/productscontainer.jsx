4c1eb72ac172225fa5158a9fdeb02c4962755a3e
volkan <nzaro19@gmail.com>
2024-08-11 02:25:54 +0300

    added products container

--- src/app/components/productscontainer.jsx
+++ src/app/components/productscontainer.jsx
@@ -1,8 +1,11 @@
 import Pagination from "./pagination"
+import ProductCards from "./productcards"
+
 
 export default function ProductsContainer() {
     return (
         <section className="container mx-auto">
+            <ProductCards />
             <Pagination />
         </section>
     )
