9c38f4ff1a3dff79cc2b00f47a86fa6758aa67e8
volkan <nzaro19@gmail.com>
2024-08-11 16:41:23 +0300

    added header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Footer from "./components/footer";
 import Prefooter from "./components/prefooter";
 import ProductsContainer from "./components/productscontainer"
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <>
+    <Header />
       <main className="flex flex-col items-center justify-between min-h-screen p-24">
         <ProductsContainer />
         <Prefooter />
